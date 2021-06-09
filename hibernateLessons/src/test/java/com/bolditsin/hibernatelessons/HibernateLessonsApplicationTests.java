10acf1bd6307128fb8231b7771a2cc6b6bab80a1
bolditsin <70659316+bolditsin@users.noreply.github.com>
2021-06-09 19:11:46 +0300

    Д/з по Spring Hibernate урок 1.

---
+++ hibernateLessons/src/test/java/com/bolditsin/hibernatelessons/HibernateLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolditsin.hibernatelessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateLessonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
